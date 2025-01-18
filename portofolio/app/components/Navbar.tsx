cc63cd8cff660a965f305cbc6a2a6944bdee37d9
MPolyzoidis <polyzoidism@gmail.com>
2025-01-18 20:32:25 +0200

    Add Background and fix some styling

--- portofolio/app/components/Navbar.tsx
+++ portofolio/app/components/Navbar.tsx
@@ -1,11 +1,11 @@
 const Navbar = () => {
     return (
-        <div className="flex justify-center items-center relative top-3">
+        <div className="flex justify-center items-center fixed w-full top-3 z-50">
             <nav className="flex gap-1 p-0.5 border border-white/15 rounded-lg bg-white/10 backdrop-blur">
                 <a href="#" className="nav-item">Home</a>
                 <a href="#" className="nav-item">About</a>
                 <a href="#" className="nav-item">Projects</a>
-                <a href="#" className="nav-item">Contacts</a>
+                <a href="#" className="nav-item">Contact</a>
             </nav>
         </div>
     )
--- portofolio/app/sections/HeroSection.tsx
+++ portofolio/app/sections/HeroSection.tsx
@@ -1,40 +1,51 @@
 import memojiImage from "../images/memoji.png"
 import ArrowDown from "../images/downArrow.svg"
 import Image from 'next/image'
+import background from '../images/background.png'
 
 const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+
+        {/* Background Image */}
+        <Image 
+            src={background} 
+            alt="Background" 
+            className="absolute inset-0 -z-30 opacity-5 h-screen max-w-[1920px] overflow-hidden">        
+        </Image>
+
         <div className='container'>
 
             <div className="flex flex-col items-center">
                 <Image 
                     src={memojiImage} 
                     alt="Person peeking from behind laptop" 
-                    className="size-[100px]"
+                    className="h-28 w-auto"
                 />
 
-                <div className="bg-gray-950 border border-gray-800 rounded-lg px-1.5 py-1.5">
-                    <div className="text-sm">Marios Polyzoidis</div>
+                <div className="bg-gray-950 border border-gray-800 rounded-lg px-4 py-1.5">
+                    <div className="text-sm font-medium">Marios Polyzoidis</div>
                 </div>
             </div>
+            
+            <div className="max-w-lg mx-auto">
+                <h1 className="text-3xl md:text-5xl text-center mt-8 font-bold tracking-wide">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
 
-            <h1 className="text-3xl text-center mt-8 font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+                <p className="mt-4 text-center text-white/60 md:text-lg"> 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p> 
+            </div>
 
-            <p> 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p> 
-
-            <div>
-                <button>
-                    <span>Explore My Work</span>
-                    <ArrowDown className="fill-white"/>
+            <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+                <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+                    <span className="font-semibold">Explore My Work</span>
+                    <ArrowDown className="fill-white size-4"/>
                 </button>
 
-                <button>
+                <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-800 px-6 h-12 rounded-xl">
                     <span>👋</span>
-                    <span>Contact Me</span>
+                    <span className="font-semibold">Contact Me</span>
                 </button>
             </div>   
 
