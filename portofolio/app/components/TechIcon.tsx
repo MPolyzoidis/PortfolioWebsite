5c23ea213f37c024efd02b960643c7e03d17e133
MPolyzoidis <polyzoidism@gmail.com>
2025-01-22 21:39:51 +0200

    Create TechIcon Reusable Component and change some stuff

---
+++ portofolio/app/components/TechIcon.tsx
@@ -0,0 +1,10 @@
+const TechIcon = ( { component } : { component: React.ElementType } ) => {
+
+    const Component = component;
+
+    return (
+        <Component className="size-10 fill-white"/>
+    )
+}
+
+export default TechIcon
--- portofolio/app/sections/About.tsx
+++ portofolio/app/sections/About.tsx
@@ -1,9 +1,153 @@
+//Reusable Component Imports
 import Background from "../components/Background"
 import Card from "../components/Card"
 import SectionHeader from "../components/SectionHeader"
-import ArrowDown from "../images/downArrow.svg"
+
+//Images Imports
 import Image from 'next/image'
+import ArrowDown from "../images/downArrow.svg"
 import placeholder from "../images/placeholder.png"
+import mapImage from "../images/map.png"
+import memojiSmile from "../images/memoji-smile.png"
+
+//Toolbox Icons Imports
+import CSSIcon from "../images/TechIcons/css3-icon.svg"
+import DockerIcon from "../images/TechIcons/docker-icon.svg"
+import FigmaIcon from "../images/TechIcons/figma-icon.svg"
+import FlutterIcon from "../images/TechIcons/flutter-icon.svg"
+import GitHubIcon from "../images/TechIcons/github-icon.svg"
+import GitLabIcon from "../images/TechIcons/gitlab-icon.svg"
+import HTMLIcon from "../images/TechIcons/html5-icon.svg"
+import JavaIcon from "../images/TechIcons/java-icon.svg"
+import CSharpIcon from "../images/TechIcons/c-sharp-icon.svg"
+import JavaScriptIcon from "../images/TechIcons/js-icon.svg"
+import TypeScriptIcon from "../images/TechIcons/ts-icon.svg"
+import MySQLIcon from "../images/TechIcons/mysql-icon.svg"
+import PostgreSQLIcon from "../images/TechIcons/postgresql-icon.svg"
+import NodeIcon from "../images/TechIcons/node-icon.svg"
+import ExpressIcon from "../images/TechIcons/express-icon.svg"
+import ReactIcon from "../images/TechIcons/react-icon.svg"
+import TailwindIcon from "../images/TechIcons/tailwind-icon.svg"
+import TrelloIcon from "../images/TechIcons/trello-icon.svg"
+import UnityIcon from "../images/TechIcons/unity-icon.svg"
+import ViteIcon from "../images/TechIcons/vite-icon.svg"
+import NextIcon from "../images/TechIcons/next-icon.svg"
+import TechIcon from "../components/TechIcon"
+
+
+const toolboxItems = [
+    {
+        title: "HTML",
+        iconType: HTMLIcon 
+    },
+    {
+        title: "CSS",
+        iconType: CSSIcon 
+    },
+    {
+        title: "JavaScript",
+        iconType: JavaScriptIcon 
+    },
+    {
+        title: "TypeScript",
+        iconType: TypeScriptIcon 
+    },
+    {
+        title: "React",
+        iconType: ReactIcon 
+    },
+    {
+        title: "Vite",
+        iconType: ViteIcon 
+    },
+    {
+        title: "Next.js",
+        iconType: NextIcon 
+    },
+    {
+        title: "Tailwind CSS",
+        iconType: TailwindIcon 
+    },
+    {
+        title: "GitHub",
+        iconType: GitHubIcon 
+    },
+    {
+        title: "GitLab",
+        iconType: GitLabIcon 
+    },
+    {
+        title: "Java",
+        iconType: JavaIcon 
+    },
+    {
+        title: "C#",
+        iconType: CSharpIcon 
+    },
+    {
+        title: "Flutter",
+        iconType: FlutterIcon 
+    },
+    {
+        title: "MySQL",
+        iconType: MySQLIcon 
+    },
+    {
+        title: "PostgreSQL",
+        iconType: PostgreSQLIcon 
+    },
+    {
+        title: "Node.js",
+        iconType: NodeIcon 
+    },
+    {
+        title: "Express",
+        iconType: ExpressIcon 
+    },
+    {
+        title: "Figma",
+        iconType: FigmaIcon 
+    },
+    {
+        title: "Docker",
+        iconType: DockerIcon 
+    },
+    {
+        title: "Unity",
+        iconType: UnityIcon 
+    },
+    {
+        title: "Trello",
+        iconType: TrelloIcon 
+    }
+]
+
+const hobbies = [
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+    {
+        title:"Ipsum",
+        emoji:"⚽"
+    },
+]
 
 
 const AboutSection = () => {
@@ -42,7 +186,45 @@ const AboutSection = () => {
                 <h3>My Toolbox</h3>
                 <p>Explore the technologies I use to create exceptional digital experiences</p>
 
+                <div>
+
+                    {toolboxItems.map(item => (
+
+                        <div key={item.title}>
+
+                            <TechIcon component={item.iconType} />
+                            <span>{item.title}</span>
+
+                        </div>
+
+                    ))}
+
+                </div>
+
             </div>
+        </Card>
+
+        <Card>
+            <div>
+                <ArrowDown className="fill-white size-4"/>
+
+                <h3>Beyond the Code</h3>
+                <p>Explore my interests and hobbies beyond the digital realm</p>
+            </div>
+
+            <div>
+                {hobbies.map(hobby => (
+                    <div key={hobby.title}>
+                        <span>{hobby.title}</span>
+                        <span>{hobby.emoji}</span>
+                    </div>
+                ))}
+            </div>
+        </Card>
+
+        <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={memojiSmile} alt="Smiling memoji" />
         </Card>
 
     </div>
