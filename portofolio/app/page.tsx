912cb0a3e338e24d08410d02ebf3cd565924a17f
MPolyzoidis <polyzoidism@gmail.com>
2025-01-27 19:47:53 +0200

    Add FooterSection

--- portofolio/app/page.tsx
+++ portofolio/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import AboutSection from "./sections/About";
 import ContactSection from "./sections/Contact";
+import FooterSection from "./sections/Footer";
 import HeroSection from "./sections/Hero";
 import ProjectsSection from "./sections/Projects";
 import TestimonialsSection from "./sections/Testimonials";
@@ -14,6 +15,7 @@ export default function Home() {
             <TestimonialsSection />
             <AboutSection />
             <ContactSection />
+            <FooterSection />
         </div>
     );
 }
--- portofolio/app/sections/Contact.tsx
+++ portofolio/app/sections/Contact.tsx
@@ -2,34 +2,36 @@ import Background from "../components/Background"
 import ArrowUpRight from "../images/arrow-up-right-from-square-solid.svg"
 
 const ContactSection = () => {
-  return (
+    return (
 
-    <div className="py-16 pt-12 lg:py-24 lg:pt-20 relative">
+        <div className="py-16 pt-12 lg:py-24 lg:pt-20 relative">
 
-        <div className="container">
+            <div className="container">
 
-            <Background />
+                <Background />
 
-            <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left">
 
-                <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+                    <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
 
-                    <div>
+                        <div>
 
-                        <h2 className="font-serif text-2xl md:text-3xl">Lorem Ipsum Lorem Ipsum</h2>
+                            <h2 className="font-serif text-2xl md:text-3xl">Lorem Ipsum Lorem Ipsum</h2>
 
-                        <p className="text-sm md:text-base mt-2">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                        </p>
+                            <p className="text-sm md:text-base mt-2">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                            </p>
 
-                    </div>
+                        </div>
 
-                    <div>
+                        <div>
 
-                        <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
-                            <span className="font-semibold">Contact Me</span>
-                            <ArrowUpRight className="size-4 fill-white"/>
-                        </button>
+                            <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                                <span className="font-semibold">Contact Me</span>
+                                <ArrowUpRight className="size-4 fill-white"/>
+                            </button>
+
+                        </div>
 
                     </div>
 
@@ -38,9 +40,7 @@ const ContactSection = () => {
             </div>
 
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default ContactSection
